9f9c22e18dca36b4af70b671adb968c09dc114f2
Ivan Matyazh <ivan070297@gmail.com>
2024-01-13 13:04:46 +0100

    Add close button

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function showModal(name) {
-    var modal = document.getElementById(name)
+    var modal = document.getElementById(name);
     modal.classList.add("modal--show");
+}
+
+function closeModal(name) {
+    var modal = document.getElementById(name);
+    modal.classList.remove("modal--show");
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    event.target.classList.remove("modal--show")
+    event.target.classList.remove("modal--show");
 }
